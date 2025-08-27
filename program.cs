29941e4430769ba3b674e0197b171f4a27e53721
luisecarrillom <luisecarrillom@gmail.com>
2025-08-26 21:39:44 -0600

    Initial commit

---
+++ program.cs
@@ -0,0 +1,12 @@
+﻿// Print "Hello World!" and move to a new line
+Console.WriteLine("Hello World!");
+// Print a second message and move to a new line
+Console.WriteLine("This is the second line");
+// Print a third message and move to a new line
+Console.WriteLine("This is the Third Line");
+// Print "Congratulations!" but stay on the same line
+Console.Write("Congratulations!");
+// Print a space without moving to a new line
+Console.Write(" ");
+// Print another message on the same line as "Congratulations!"
+Console.Write("You wrote your first lines of code.");
