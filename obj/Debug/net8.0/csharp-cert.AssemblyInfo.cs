cd7f474bb2f3ef6cb2b6530a907fc2cd5c172af0
luisecarrillom <luisecarrillom@gmail.com>
2026-01-15 00:08:29 -0700

    Implement Exception Handling in C# Console Applications

--- obj/Debug/net8.0/csharp-cert.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharp-cert.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-cert")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9787760d54d029c4c2733a8b27762d66e4cb147d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abe00d257db3de9ffe36da59f126f87f7bf53bc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-cert")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-cert")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- program.cs
+++ program.cs
@@ -636,3 +636,40 @@ string [] students = new string [] {"Sophia" , "Nicolas", "Andrea" , "Luis"};
 int studentCount = students.Length;
 
 Console.WriteLine("The final name is: " + students[studentCount -1]); // we need to add -1 for get access to the final name of the array
+
+// Implement Exception Handling in C# Console Applications
+
+try
+{
+    Process1();
+}
+catch
+{
+    Console.WriteLine("An exception has occurred");
+}
+
+Console.WriteLine("Exit program");
+
+static void Process1()
+{
+    try
+    {
+        WriteMessage();
+    }
+    catch
+    {
+        Console.WriteLine("Exception caught in Process1");
+    }
+
+}
+
+static void WriteMessage()
+{
+    double float1 = 3000.0;
+    double float2 = 0.0;
+    int number1 = 3000;
+    int number2 = 0;
+
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
+}
